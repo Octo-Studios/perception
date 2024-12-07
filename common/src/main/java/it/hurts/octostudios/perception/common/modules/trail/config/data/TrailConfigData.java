8e17f859bd345ceeae7ff6208cea10abaf517d76
SSKirillSS <SSTroveSS@Gmail.com>
2024-12-07 14:25:55 +0300

    Added comments to the trails config data

--- common/src/main/java/it/hurts/octostudios/perception/common/modules/trail/config/data/TrailConfigData.java
+++ common/src/main/java/it/hurts/octostudios/perception/common/modules/trail/config/data/TrailConfigData.java
@@ -1,5 +1,6 @@
 package it.hurts.octostudios.perception.common.modules.trail.config.data;
 
+import it.hurts.octostudios.octolib.modules.config.annotations.Prop;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,11 +10,18 @@ import org.joml.Vector3f;
 @NoArgsConstructor
 @AllArgsConstructor
 public class TrailConfigData {
+    @Prop(comment = "Trail thickness")
     private float size = 0.1F;
+    @Prop(comment = "Maximum trail length, measured in points that are placed every N ticks, where N is the value of updateFrequency")
     private int maxPoints = 5;
+    @Prop(comment = "Minimum object movement speed to place trail points")
     private float minSpeed = 0.001F;
+    @Prop(comment = "Frequency of trail point placement")
     private int updateFrequency = 1;
+    @Prop(comment = "Starting color of the trail in ARGB format")
     private String fadeInColor = "FFFFFFFF";
+    @Prop(comment = "Ending color of the trail in ARGB format")
     private String fadeOutColor = "FFFFFFFF";
+    @Prop(comment = "Position offset of the trail relative to the object in XYZ")
     private Vector3f positionOffset = new Vector3f(0F, 0F, 0F);
 }
