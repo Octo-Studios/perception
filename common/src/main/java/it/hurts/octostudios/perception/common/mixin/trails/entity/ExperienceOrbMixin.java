783a65ffc04a6649b7cbf0683f21d7ed7be574b1
SSKirillSS <SSTroveSS@Gmail.com>
2024-12-14 18:01:15 +0300

    Implemented dynamic trail scale for experience orbs

--- common/src/main/java/it/hurts/octostudios/perception/common/mixin/trails/entity/ExperienceOrbMixin.java
+++ common/src/main/java/it/hurts/octostudios/perception/common/mixin/trails/entity/ExperienceOrbMixin.java
@@ -1,11 +1,7 @@
 package it.hurts.octostudios.perception.common.mixin.trails.entity;
 
 import net.minecraft.world.entity.ExperienceOrb;
-import net.minecraft.world.entity.item.ItemEntity;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Rarity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(ExperienceOrb.class)
 public abstract class ExperienceOrbMixin extends EntityMixin {
@@ -13,6 +9,8 @@ public abstract class ExperienceOrbMixin extends EntityMixin {
     public double getTrailScale() {
         var entity = (ExperienceOrb) (Object) this;
 
-        return super.getTrailScale() * ((entity.getIcon() + 1) / 3F);
+        var base = super.getTrailScale();
+
+        return base + (base * entity.getIcon() * 0.015F);
     }
 }
