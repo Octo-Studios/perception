06b9cd7d9c3ad0d18d612a25d5a0ab7c0fd3fde1
SSKirillSS <SSTroveSS@Gmail.com>
2024-12-22 15:26:38 +0300

    Fixed particles trails render conditions

--- common/src/main/java/it/hurts/octostudios/perception/common/mixin/trails/particle/ParticleMixin.java
+++ common/src/main/java/it/hurts/octostudios/perception/common/mixin/trails/particle/ParticleMixin.java
@@ -61,8 +61,7 @@ public class ParticleMixin implements ITrailConfigProvider {
 
     @Override
     public boolean isTrailGrowing() {
-//        return new Vec3(x, y, z).distanceTo(new Vec3(xo, yo, zo)) >= getTrailConfigData().getMinSpeed();
-        return true;
+        return new Vec3(x, y, z).distanceTo(new Vec3(xo, yo, zo)) >= getTrailConfigData().getMinSpeed();
     }
 
     @Override
